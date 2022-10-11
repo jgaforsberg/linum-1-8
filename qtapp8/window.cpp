3e7cf73c0492369462f72f64f5a197f4fc2d1c69
jgaforsberg <jaforsberg@gmail.com>
2022-10-11 10:26:36 +0200

    exempelprogram1

--- qtapp8/window.cpp
+++ qtapp8/window.cpp
@@ -1,13 +1,17 @@
 #include "window.h"
+#include <QApplication>
 #include <QPushButton>
 
 Window::Window(QWidget *parent) : QWidget(parent)
 {
     // set window size
-    setFixedSize(100, 50);
+    setFixedSize(200, 100);
     //create and position button
     m_button = new QPushButton("Hello World!", this);
-    m_button->setGeometry(10,10,80,30);
+    m_button->setGeometry(20,20,160,60);
     // with parenting system destructor is unneccessary since instances are automatically deleted
+
+    // connection for button
+    connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
 }
 
