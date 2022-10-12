eedacc851b7af429e03dfe5be3c590c1527b6ba6
jgaforsberg <jaforsberg@gmail.com>
2022-10-12 10:52:25 +0200

    quit-funktion

--- deluppgift1/window.cpp
+++ deluppgift1/window.cpp
@@ -1,4 +1,5 @@
 #include "window.h"
+#include <QApplication>
 #include <QPushButton>
 
 Window::Window(QWidget *parent) : QWidget(parent)
@@ -6,8 +7,14 @@ Window::Window(QWidget *parent) : QWidget(parent)
     // set window size
     setFixedSize(1280, 720);
 
-    // set and position button
+    // set and position buttons
     quitButton = new QPushButton("Avsluta", this);
     quitButton->setGeometry(580, 600, 100, 50);
+    quitButton->setCheckable(true);
+
+
+    // button connections
+    connect (quitButton, SIGNAL(clicked()), QApplication::instance(), SLOT(quit()));
+
 }
 
